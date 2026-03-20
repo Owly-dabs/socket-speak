b4f1cf3f80a7dc7be8e573579af62f0aaff8b9a2
Benny Goh <bennyrobot1143@gmail.com>
2026-03-20 04:26:54 +0000

    add example meow_commands

--- commands_registry.h
+++ commands_registry.h
@@ -8,6 +8,7 @@ typedef enum
     LMP_MSG = 0x01,
     LMP_NICK = 0x02,
     LMP_ACK = 0x03,
+    LMP_MEOW = 0x10,
     LMP_ERROR = 0xFF
 } LMPCode;
 
---
+++ meow_commands.h
@@ -0,0 +1,7 @@
+/* meow_commands.h */
+#ifndef MEOW_COMMANDS_H
+#define MEOW_COMMANDS_H
+
+void meow_commands_init(void);
+
+#endif /* MEOW_COMMANDS_H */
