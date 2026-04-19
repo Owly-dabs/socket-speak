3cf2c350762db9b1d8e3c9bb6f6a8b7ed09e390d
Benny Goh <bennyrobot1143@gmail.com>
2026-04-19 12:15:26 +0000

    change LMP_STICKERS to 0x05

--- commands_registry.h
+++ commands_registry.h
@@ -9,7 +9,7 @@ typedef enum
     LMP_NICK = 0x02,
     LMP_ACK = 0x03,
     LMP_MEOW = 0x10,
-    LMP_STICKERS = 0x11,
+    LMP_STICKERS = 0x05,
     LMP_ERROR = 0xFF,
     LMP_UID = 0x04,
     LMP_GRP_OBJ = 0x80,         /* Server > User */
